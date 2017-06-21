f7eccc1afb21035268e0a6233669a0835cb21d4e
Soichiro-Yoshimura <yoshimura@soichiro.org>
2017-06-21 22:43:31 +0900

    First Commit

---
+++ src/main/scala/TenThousandNamePrinter.scala
@@ -0,0 +1,10 @@
+object TenThousandNamePrinter extends App {
+
+  for (i <- 1 to 10000) {
+    new Thread(() => {
+      Thread.sleep(1000)
+      println(Thread.currentThread().getName)
+    }).start()
+  }
+
+}
