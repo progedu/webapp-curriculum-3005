83619346ad830572e43cce6343ba53d62d5511ab
murosan <masashi_16n1200013@nnn.ed.jp>
2017-10-01 11:41:16 +0900

    初級

--- src/main/scala/TenThousandNamePrinter.scala
+++ src/main/scala/TenThousandNamePrinter.scala
@@ -1,10 +1,17 @@
-object TenThousandNamePrinter extends App {
+import java.util.concurrent.{Callable, Executors}
 
-  for (i <- 1 to 10000) {
-    new Thread(() => {
-      Thread.sleep(1000)
-      println(Thread.currentThread().getName)
-    }).start()
+object TenThousandNamePrinter extends App {
+  val es = Executors.newFixedThreadPool(1000)
+
+  val printers = for (i <- 1 to 10000) yield {
+    es.submit(new Callable[Unit] {
+      override def call(): Unit = {
+        Thread.sleep(1000)
+        println(Thread.currentThread().getName)
+      }
+    })
   }
 
+  printers.foreach(_.get())
+  es.shutdownNow()
 }
