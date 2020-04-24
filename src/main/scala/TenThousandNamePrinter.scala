d92e5a35f0c0705fdef7b7e1e780499c062fca1c
Matsuda Kazuyuki <mtdkzyk.github@gmail.com>
2020-04-24 19:57:44 +0900

    Use Executor framework

--- src/main/scala/TenThousandNamePrinter.scala
+++ src/main/scala/TenThousandNamePrinter.scala
@@ -1,10 +1,21 @@
+import java.util.concurrent.{Executors, TimeUnit}
+import java.util.concurrent.atomic.AtomicInteger
+
 object TenThousandNamePrinter extends App {
 
-  for (i <- 1 to 10000) {
-    new Thread(() => {
-      Thread.sleep(1000)
-      println(Thread.currentThread().getName)
-    }).start()
+  val es = Executors.newFixedThreadPool(200)
+  val counter = new AtomicInteger(0)
+
+  val threads = for (i <- 1 to 10000) yield {
+    es.submit(new Runnable {
+      override def run(): Unit = {
+        val count = counter.incrementAndGet()
+        Thread.sleep(1000)
+        println(s"${count} ${Thread.currentThread().getName}")
+      }
+    })
   }
+
+  es.shutdown()
 
 }
