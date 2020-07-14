3963a5f5a4c877291822cb71afa9fbe1be25b646
Yuta Takaba <yutakaba@gmail.com>
2020-07-15 00:21:25 +0900

    use executor

--- src/main/scala/TenThousandNamePrinter.scala
+++ src/main/scala/TenThousandNamePrinter.scala
@@ -1,10 +1,13 @@
+import java.util.concurrent.Executors
+
 object TenThousandNamePrinter extends App {
+  val es = Executors.newFixedThreadPool(10)
 
   for (i <- 1 to 10000) {
-    new Thread(() => {
+    es.submit(new Thread(() => {
       Thread.sleep(1000)
       println(Thread.currentThread().getName)
-    }).start()
+    }))
   }
 
 }
